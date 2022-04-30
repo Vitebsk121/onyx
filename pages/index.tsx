114dced8f0854f7f2ca257d0e29fa10f4d4157bb
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2022-04-30 12:21:55 +0300

    feat: добавлен видеофон

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import MainLayout from "../HOC/MainLayout";
+import styles from "../styles/SPromo.module.scss"
 
 const MainPage = () => {
   return (
     <MainLayout title={'Главная'} >
-      <h1>Hello Onyx</h1>
+      <section className={styles.promo}>
+        <div className={styles.curtain}>
+          <h1 className={styles.title}>ONYX - Драгоценное качество деятельности</h1>
+        </div>
+        <video
+          className={styles.videoBackground}
+          src="/kar.mov"
+          autoPlay
+          muted
+          loop
+        />
+      </section>
     </MainLayout >
   );
 };
