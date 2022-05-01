798f79928943905531697d5b54daf3830ac21f63
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2022-05-01 17:44:05 +0300

    feat: добавлены данные для секции проекты

---
+++ data/projectsList.ts
@@ -0,0 +1,23 @@
+import AutobahnPic from "../public/images/autobahn.png";
+import StroyGazPic from "../public/images/stroyGaz.png";
+import CardPic3 from "../public/images/project3.png";
+
+const projectsList = [
+  {
+    image: AutobahnPic,
+    title: "Автобан М-12",
+    description: "Создание дизайна для брендов и вовлечение людей, которые с ними взаимодействуют, — одна из основных целей нашей компании."
+  },
+  {
+    image: StroyGazPic,
+    title: "Стройтрансгаз",
+    description: "Создание дизайна для брендов и вовлечение людей, которые с ними взаимодействуют, — одна из основных целей нашей компании."
+  },
+  {
+    image: CardPic3,
+    title: "Ещё проект",
+    description: "Создание дизайна для брендов и вовлечение людей, которые с ними взаимодействуют, — одна из основных целей нашей компании."
+  },
+]
+
+export default projectsList;
