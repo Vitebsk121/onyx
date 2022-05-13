18347e87fc69ee86ad60b83cc0fcf5f1e5dd6667
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2022-05-13 14:11:16 +0300

    feat: слайдер изменен на новый

--- components/SectionAutopark/SectionAutopark.tsx
+++ components/SectionAutopark/SectionAutopark.tsx
@@ -2,13 +2,12 @@ import styles from "./SectionAutopark.module.scss"
 import Marquee from "react-fast-marquee";
 import Image from "next/image";
 import AutoparkLogo from "../../public/autopark.svg";
-import Slider from "../Slider/Slider";
-import React, { useRef } from "react";
+import React from "react";
 import autoparkList from "../../data/autoparkList";
+import Slider from "../Slider/Slider";
 
 
 const SectionAutopark = () => {
-  const slideNode = useRef<HTMLDivElement>(null);
   return (
     <section className={styles.autopark}>
       <Marquee speed={50} gradient={false}>
@@ -17,23 +16,15 @@ const SectionAutopark = () => {
       <div className={styles.wrapper}>
         <div className={styles.autoparkBlock}>
           <Slider
-            childrenRef={slideNode}
-            autoScrollTime={5000}
-            dotScrollIsVisible={true}
-            controls={false}
+            countOfVisibleElements={1}
+            infinity={true}
+            indicators={true}
+            autoScroll={{time: 3000, orientation: 'right'}}
           >
             {autoparkList.map(({image}, index) => (
-              index === 0
-              ? (
-                  <div key={index} ref={slideNode} className={styles.slide}>
-                    <Image src={image} alt="truck" layout={"fill"} objectFit={"cover"}/>
-                  </div>
-                )
-              : (
-                  <div key={index} className={styles.slide}>
-                    <Image src={image} alt="truck" layout={"fill"} objectFit={"cover"}/>
-                  </div>
-                )
+              <div key={index} className={styles.slide}>
+                <Image src={image} alt="truck" layout={"fill"} objectFit={"cover"}/>
+              </div>
             ))}
           </Slider>
           <h4>
@@ -44,24 +35,15 @@ const SectionAutopark = () => {
             Cобственная ремонтная зона
           </h4>
           <Slider
-            childrenRef={slideNode}
-            autoScrollTime={5000}
-            autoScrollOrientation={'right'}
-            dotScrollIsVisible={true}
-            controls={false}
+            countOfVisibleElements={1}
+            infinity={true}
+            indicators={true}
+            autoScroll={{time: 3000, orientation: 'left'}}
           >
             {autoparkList.map(({image}, index) => (
-              index === 0
-                ? (
-                  <div key={index} ref={slideNode} className={styles.slide}>
-                    <Image src={image} alt="truck" layout={"fill"} objectFit={"cover"}/>
-                  </div>
-                )
-                : (
-                  <div key={index} className={styles.slide}>
-                    <Image src={image} alt="truck" layout={"fill"} objectFit={"cover"}/>
-                  </div>
-                )
+              <div key={index} className={styles.slide}>
+              <Image src={image} alt="truck" layout={"fill"} objectFit={"cover"}/>
+              </div>
             ))}
           </Slider>
         </div>
